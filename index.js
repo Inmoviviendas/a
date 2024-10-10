050113545bf3193d6747e2fea2ce39ea9763cd8a
Inmoviviendas <inmoviviendasmelilla@gmail.com>
2024-10-10 12:17:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+onst express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('El servidor está funcionando correctamente');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor funcionando en el puerto ${port}`);
+});
